20ed6c52020ae0b2e6d98d400ea660c0caa7e83c
akejuay <akejuayomide333@gmail.com>
2026-09-04 23:59:03 +0100

    Updated dashboard

--- middleware.js
+++ middleware.js
@@ -1,3 +1,15 @@
-export {default} from "next-auth/middleware"
+// export {default} from "next-auth/middleware"
 
-export const config = {matcher: ["/dashboard"]}
+// export const config = {matcher: ["/dashboard"]}
+
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/",
+  },
+});
+
+export const config = {
+  matcher: ["/dashboard/:path*"],
+};
